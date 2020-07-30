7ef76837beb404bcd288a3183bef933c815b18ea
github username <github email address>
2020-07-30 15:16:07 -0400

    needs alot of work..

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,46 +26,18 @@ u2 = User.create(name: "Lena", username: "lena", password_digest: "word")
 # l4 = Log.create(user_id: u2.id, title: "d", start: "2020-07-12", water: 14, mood: 5, note: "Passed my interview!")
 # l5 = Log.create(user_id: u1.id, title: "e", start: "2020-07-16", water: 9, mood: 3, note: "Okay..")
 
-l1 = Log.create(user_id: u1.id, title: "Went to gym", start: "2020-07-15", water: 10, mood: 4)
+l1 = Log.create(user_id: u1.id, title: "Went to gym", start: "2020-07-15", water: 17, mood: 4)
 l2 = Log.create(user_id: u2.id, title: "Someone broke into my car", start: "2020-07-13", water: 4, mood: 2)
-l3 = Log.create(user_id: u1.id, title: "Broke my legs", start: "2020-07-10", water: 3, mood: 1)
-l4 = Log.create(user_id: u2.id, title: "Passed my interview!", start: "2020-07-12", water: 14, mood: 5)
-l5 = Log.create(user_id: u1.id, title: "Rachel...", start: "2020-07-16", water: 2, mood: 1)
+l3 = Log.create(user_id: u1.id, title: "Could barely sleep", start: "2020-07-10", water: 3, mood: 2)
+l4 = Log.create(user_id: u2.id, title: "Passed my interview!", start: "2020-07-12", water: 20, mood: 5)
+l5 = Log.create(user_id: u1.id, title: "Josh passed away.", start: "2020-07-16", water: 2, mood: 1)
 
-l6 = Log.create(user_id: u1.id, title: "Passed my exam", start: "2020-08-12", water: 12, mood: 5)
-l7 = Log.create(user_id: u1.id, title: "Fishing", start: "2020-08-16", water: 9, mood: 3)
-l8 = Log.create(user_id: u1.id, title: "Got my liscence", start: "2020-07-17", water: 13, mood: 4)
+
+
+l6 = Log.create(user_id: u1.id, title: "Passed my exam", start: "2020-07-18", water: 12, mood: 5)
+l7 = Log.create(user_id: u1.id, title: "Fishing at pier", start: "2020-07-16", water: 9, mood: 3)
+l8 = Log.create(user_id: u1.id, title: "Obtained a driver liscence", start: "2020-07-17", water: 13, mood: 4)
 l9 = Log.create(user_id: u1.id, title: "New kicks", start: "2020-07-21", water: 2, mood: 4)
-
-
-s1 = Sleep.create(log_id: l1.id, start_time: "22:00", end_time: "7:00")
-s2 = Sleep.create(log_id: l2.id, start_time: "20:00", end_time: "5:00")
-s3 = Sleep.create(log_id: l3.id, start_time: "18:00", end_time: "6:00")
-s4 = Sleep.create(log_id: l4.id, start_time: "23:00", end_time: "1:00")
-s5 = Sleep.create(log_id: l5.id, start_time: "21:00", end_time: "4:00")
-
-s6 = Sleep.create(log_id: l5.id, start_time: "5:00", end_time: "6:00")
-s7 = Sleep.create(log_id: l5.id, start_time: "6:30", end_time: "7:00")
-s8 = Sleep.create(log_id: l1.id, start_time: "7:30", end_time: "9:00")
-
-m1 = Meal.create(log_id: l1.id, time: "8:00", name: "hot dog")
-m2 = Meal.create(log_id: l2.id, time: "2:00", name: "pizza")
-m3 = Meal.create(log_id: l3.id, time: "4:00", name: "rice")
-m4 = Meal.create(log_id: l4.id, time: "23:00", name: "burger")
-m5 = Meal.create(log_id: l5.id, time: "14:00", name: "duck")
-
-m6 = Meal.create(log_id: l1.id, time: "13:00", name: "ramen")
-m7 = Meal.create(log_id: l1.id, time: "19:00", name: "fries")
-m8 = Meal.create(log_id: l2.id, time: "10:00", name: "cereal")
-m9 = Meal.create(log_id: l4.id, time: "7:00", name: "french toast")
-
-m10 = Meal.create(log_id: l8.id, time: "4:00", name: "cake")
-m11 = Meal.create(log_id: l8.id, time: "23:00", name: "grilled cheese")
-m12 = Meal.create(log_id: l9.id, time: "14:00", name: "chocolate")
-m13 = Meal.create(log_id: l7.id, time: "23:00", name: "burger")
-m14 = Meal.create(log_id: l7.id, time: "13:00", name: "sushi")
-
-
 
 # s1 = Sleep.create(log_id: l1.id, quantity: 3)
 # s2 = Sleep.create(log_id: l2.id, quantity: 4)
@@ -78,6 +50,40 @@ m14 = Meal.create(log_id: l7.id, time: "13:00", name: "sushi")
 # m3 = Meal.create(log_id: l3.id, quantity: 3)
 # m4 = Meal.create(log_id: l4.id, quantity: 1)
 # m5 = Meal.create(log_id: l5.id, quantity: 3)
+
+
+
+s1 = Sleep.create(log_id: l1.id, start_time: "20:00", end_time: "06:00")
+s2 = Sleep.create(log_id: l2.id, start_time: "20:00", end_time: "02:00")
+s3 = Sleep.create(log_id: l3.id, start_time: "18:00", end_time: "19:00")
+s4 = Sleep.create(log_id: l4.id, start_time: "23:00", end_time: "01:00")
+s5 = Sleep.create(log_id: l5.id, start_time: "21:00", end_time: "04:00")
+
+s6 = Sleep.create(log_id: l6.id, start_time: "15:00", end_time: "02:00")
+s7 = Sleep.create(log_id: l7.id, start_time: "20:30", end_time: "07:00")
+s8 = Sleep.create(log_id: l8.id, start_time: "03:30", end_time: "09:00")
+
+m1 = Meal.create(log_id: l1.id, time: "08:00", name: "hot dog")
+m2 = Meal.create(log_id: l2.id, time: "02:00", name: "pizza")
+m3 = Meal.create(log_id: l3.id, time: "04:00", name: "rice")
+m4 = Meal.create(log_id: l4.id, time: "23:00", name: "burger")
+m9 = Meal.create(log_id: l4.id, time: "07:00", name: "french toast")
+m5 = Meal.create(log_id: l5.id, time: "14:00", name: "duck")
+
+m6 = Meal.create(log_id: l1.id, time: "13:00", name: "ramen")
+m7 = Meal.create(log_id: l1.id, time: "19:00", name: "fries")
+m8 = Meal.create(log_id: l2.id, time: "10:00", name: "cereal")
+m15 = Meal.create(log_id: l4.id, time: "07:00", name: "french toast")
+
+m10 = Meal.create(log_id: l8.id, time: "04:00", name: "cake")
+m11 = Meal.create(log_id: l8.id, time: "23:00", name: "grilled cheese")
+m12 = Meal.create(log_id: l9.id, time: "14:00", name: "chocolate")
+m13 = Meal.create(log_id: l7.id, time: "23:00", name: "burger")
+m14 = Meal.create(log_id: l7.id, time: "13:00", name: "sushi")
+m16 = Meal.create(log_id: l1.id, time: "19:00", name: "fries")
+
+
+
 
 
 # m6 = Meal.create(log_id: l1.id, food: "chicken", time: )
