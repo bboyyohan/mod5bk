6c6855c8ba4f9a07dc988e0d26ae947fd7748080
github username <github email address>
2020-07-30 05:41:00 -0400

    w

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -82,39 +82,9 @@ class LogsController < ApplicationController
 
     def update
         log = Log.find(params[:id])
-
-        if params[:sleep_start] && params[:sleep_end]
-            if params[:sleep_start2] && params[:sleep_end2]
-                if params[:sleep_start3] && params[:sleep_end3]
-                    log.sleeps.update(start_time: params[:sleep_start], end_time: params[:sleep_end])
-                    log.sleeps.update(start_time: params[:sleep_start2], end_time: params[:sleep_end2])
-                    log.sleeps.update(start_time: params[:sleep_start3], end_time: params[:sleep_end3])
-                end
-            else
-                log.sleeps.update(start_time: params[:sleep_start], end_time: params[:sleep_end])
-                log.sleeps.update(start_time: params[:sleep_start2], end_time: params[:sleep_end2])
-            end
-        else
-            log.sleeps.update(start_time: params[:sleep_start], end_time: params[:sleep_end])
-
-        end
-
-        if params[:meal_name] && params[:meal_time]
-            if params[:meal_name2] && params[:meal_time2]
-                if params[:meal_name3] && params[:meal_time3]
-                    log.meals.update(name: params[:meal_name], time: params[:meal_time])
-                    log.meals.update(name: params[:meal_name2], time: params[:meal_time2])
-                    log.meals.update(name: params[:meal_name3], time: params[:meal_time3])
-                end
-            else
-                log.meals.update(name: params[:meal_name], time: params[:meal_time])
-                log.meals.update(name: params[:meal_name2], time: params[:meal_time2])
-            end
-        else
-            log.meals.update(name: params[:meal_name], time: params[:meal_time])
-
-        end
+        
         log.update(log_params)
+
         render json: log.as_json(:include => [:sleeps, :meals])
     end 
 
